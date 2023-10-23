ebafeb0dbb1fcee37e8a4f708462682cb43337e8
Marcos Coelho <mparav124@gmail.com>
2023-10-23 16:01:43 -0300

    chore: time open or close feedback

--- src/components/time/Time.jsx
+++ src/components/time/Time.jsx
@@ -2,6 +2,37 @@ import React from 'react';
 import styles from './Time.module.css';
 
 const Time = () => {
+  const [isOpenGym, setIsOpenGym] = React.useState(false);
+
+  React.useEffect(() => {
+    const timeZone = 'America/Sao_Paulo';
+
+    const date = new Date();
+
+    const options = {
+      timeZone: timeZone,
+      hour12: false,
+      hour: '2-digit',
+      minute: '2-digit',
+    };
+
+    const currentTime = date.toLocaleString('pt-BR', options);
+
+    const [hour, minute] = currentTime.split(':');
+
+    if (
+      (hour >= 6 && hour < 12) ||
+      (hour === 12 && minute === 0) ||
+      (hour === 13 && minute >= 30) ||
+      (hour >= 14 && hour < 22)
+    ) {
+      return setIsOpenGym(true);
+    }
+
+    console.log('Academia está fechada.');
+    setIsOpenGym(false);
+  }, []);
+
   return (
     <section className={styles.container}>
       <div className={styles.content} data-aos="fade-right">
@@ -14,12 +45,16 @@ const Time = () => {
       </div>
 
       <div
-        className={`${styles.feedback} ${styles['close']}`}
+        className={`${styles.feedback} ${
+          isOpenGym ? styles['open'] : styles['close']
+        }`}
         data-aos="fade-up"
         data-aos-delay="100"
       >
-        <h2 className={styles.feedbackText}>Fechado</h2>
-        <span className={styles.feedbackDecoration}></span>
+        <h2 className={styles.feedbackText}>
+          {isOpenGym ? 'Estamos abertos :)' : 'Estamos fechados :('}
+        </h2>
+        {/* <span className={styles.feedbackDecoration}></span> */}
       </div>
     </section>
   );
